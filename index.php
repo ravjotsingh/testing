7f7652d46c954b735a6ed5f8e515cd6bddff1fb9
ravjotsingh9 <ravjotsingh9@yahoo.com>
2014-07-20 21:11:37 -0700

    First Commit
    Signed-off-by: ravjotsingh9 <ravjotsingh9@yahoo.com>

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE Html>
+<html>
+<head>
+    <title>SwordFix Club</title>
+    <link href="index.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+ <div class="header">
+    SwordFix Club
+ </div>
+ <div class="content">
+     Hi friends,<br>
+     My name is Ravjot Singh.
+ </div>
+ <div class="index">
+     <a href="index.php">Home</a>
+ </div>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ravjot
+ * Date: 20/07/14
+ * Time: 12:02 AM
+ */
+?>
+</body>
+</html>
